f019aca3464c53433e12c6ed542cdd04661311bd
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2024-09-02 12:12:25 -0400

    minor security adjustment

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
   res.send('Blood Bowl Betting App API');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
